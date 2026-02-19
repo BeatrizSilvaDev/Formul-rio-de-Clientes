0fc0425852a86838da910d1b5e7884cbd60b15c9
BeatrizSilvaDev <biasilvadev1109@gmail.com>
2026-02-18 23:04:10 -0300

    Salvamento dos cadastros

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { FlexLayoutModule} from '@angular/flex-layout'
-import { MatCardModule } from '@angular/material/card'
-import { FormsModule } from '@angular/forms'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {Cliente }from './cliente'
+import { ClientRequest } from 'http';
+import { log } from 'console';
+
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, MatCardModule, FormsModule], 
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
 export class Cadastro {
+  cliente: Cliente = Cliente.newCliente();
 
+  salvar(){
+    console.log("Dados Clientes", this.cliente);
+    
+  }
+    
 }
